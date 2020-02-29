1ec87379172217941824889882214a8d88900730
Gaëtan Haefeli <gagah98@gmail.com>
2020-02-29 11:01:54 +0100

    upgrade burger-menu

--- src/Nav.js
+++ src/Nav.js
@@ -3,12 +3,16 @@ import logo from './img/webmole_icon.png'
 
 const Nav = () => {
     return (
+        <div>
         <nav>
  <a href="/"> <img id="logo" src={logo} alt="webmole logo"></img> </a>
   <a href="/portfolio" className="nav-link">portfolio</a>
   <a href="/blog" className="nav-link">blog</a>
   <a href="/contact" className="nav-link">contact</a>
 </nav>
+<span className="menu-header">
+</span>
+</div>
     )
 }
 
