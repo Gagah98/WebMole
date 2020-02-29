d4f6d96e780d32492c9ae5b490ea1e21f66a6ab6
Gaëtan Haefeli <gagah98@gmail.com>
2020-02-29 10:34:02 +0100

    add burger menu

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import 'normalize.css'
 import './index.scss'
+import './menu.scss'
 
 
   ReactDOM.render(<App />, document.getElementById('root'))
